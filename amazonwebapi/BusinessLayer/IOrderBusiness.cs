8150af5a52cec645f25d1d35e0dcca5bccc875c6
MadhunikaLagishetty <104381581+MadhunikaLagishetty@users.noreply.github.com>
2022-05-04 22:24:43 +0530

    done

---
+++ amazonwebapi/BusinessLayer/IOrderBusiness.cs
@@ -0,0 +1,20 @@
+﻿using DomainLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public interface IOrderBusiness
+    {
+        
+            public Task<List<AmazonOrders>> GetAllAmazonOrdersOrders();
+            public Task<AmazonOrders> GetAmazonOrderById(int Id);
+            public Task InsertAmazonOrder(AmazonOrders amazonOrders);
+            public Task UpdateAmazonOrder(AmazonOrders amazonOrders);
+            public Task DeleteAmazonOrder(int Id);
+        
+    }
+}
---
+++ amazonwebapi/DataAccessLayer/IAmazonRepository.cs
@@ -0,0 +1,18 @@
+﻿using DomainLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public interface IAmazonRepository
+    {
+        public Task<List<AmazonOrders>> GetAllOrders();
+        public Task<AmazonOrders> GetOrderById(int Id);
+        public Task InsertOrder(AmazonOrders amazonOrders);
+        public Task UpdateOrder(AmazonOrders amazonOrders);
+        public Task DeleteOrderById(int Id);
+    }
+}
